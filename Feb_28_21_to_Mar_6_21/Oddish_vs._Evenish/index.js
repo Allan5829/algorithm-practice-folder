6ec5751166f2906e4b97c1140eef6f8c2b3112c5
Allan5829 <allan.farca@gmail.com>
2021-03-06 23:12:02 -0800

    checked and solved odd/even algorithm. Writes comments explaining code.

--- Feb_28_21_to_Mar_6_21/Oddish_vs._Evenish/index.js
+++ Feb_28_21_to_Mar_6_21/Oddish_vs._Evenish/index.js
@@ -1,3 +1,9 @@
 function oddishOrEvenish(num) {
-	
+    let array = num.toString().split('') // Turn array into string and then split each character into an array
+    let sum = array.reduce( (a, b) => parseInt(a, 10) + parseInt(b, 10) ) // reduce the array of values into a sum and turn each string 
+        // into an integer
+    //console.log(sum % 2 === 1 ? 'Oddish' : 'Evenish')
+    return sum % 2 === 1 ? 'Oddish' : 'Evenish'
 }
+
+oddishOrEvenish(43)
