9cfcea935bcd44d7dbaae99a2f6b59fcb8e0f0b3
Allan5829 <allan.farca@gmail.com>
2021-03-06 23:40:19 -0800

    checks and solves boomerang algorithm.

--- Feb_28_21_to_Mar_6_21/Number_of_Boomerangs/index.js
+++ Feb_28_21_to_Mar_6_21/Number_of_Boomerangs/index.js
@@ -1,3 +1,10 @@
 function countBoomerangs(arr) {
-	
+    let result
+    for (result = 0; arr.length > 2; arr.shift()) {
+        arr[0] === arr[2] && arr[0] !== arr[1] ? result++ : null
+    }
+    console.log(result)
+    return result
 }
+
+countBoomerangs([1, 7, 1, 7, 1, 7, 1]) //5
