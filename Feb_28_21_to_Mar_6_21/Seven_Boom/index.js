11b6965ce7a00768621834f2dcc2880636bc7049
Allan5829 <allan.farca@gmail.com>
2021-03-06 23:31:03 -0800

    checked and solved sevenboom algorithm.

--- Feb_28_21_to_Mar_6_21/Seven_Boom/index.js
+++ Feb_28_21_to_Mar_6_21/Seven_Boom/index.js
@@ -1,3 +1,8 @@
 function sevenBoom(arr) {
-	
+	let stringArray = arr.reduce( (a, b) => a.toString() + b.toString() ).split('')
+    let decending = stringArray.sort( (a, b) => { return a > 7 ? a - b : b - a  })
+    console.log(decending)
+    return decending[0] === '7' ? "Boom!" : "there is no 7 in the array"
 }
+
+sevenBoom([76, 55, 493456789, 32])
