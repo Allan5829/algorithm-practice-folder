f72fdb7866e3070fb8642a5d05be46b813a4cd36
Allan5829 <allan.farca@gmail.com>
2021-03-10 20:23:44 -0800

    current progress, trying to solve endless loop bug.

--- Mar_7_21_to_Mar_13_21/car_fleet_II/index.js
+++ Mar_7_21_to_Mar_13_21/car_fleet_II/index.js
@@ -3,11 +3,27 @@ var getCollisionTimes = function(cars) {
 
     for (let i = 0; i < cars.length; i++) {
         if (i === cars.length - 1) {
-            result.push(-1) // when last car
+            result.push(-1.00000) // when last car
         } else if (cars[i][1] < cars[i+1][1]) {
-            result.push(-1) // when next car is faster than current car
+            result.push(-1.00000) // when next car is faster than current car
         } else {
-            //loop?
+            console.log(cars[i], cars[i+1])
+            let currentCar = cars[i][0]
+            let nextCar = cars[i+1][0]
+            let time
+            /* current bug is currentCar never surpasses nextCar for some reason
+            will try using toFixed()
+            for (time = 0; currentCar < nextCar; time+= 0.1) {
+                currentCar = cars[i][0]
+                nextCar = cars[i+1][0]
+                currentCar += (cars[i][1] * time)
+                nextCar += (cars[i+1][1] * time)
+                roundTo(currentCar)
+                roundTo(nextCar)
+                console.log(currentCar, nextCar)
+            }
+            result.push(time)
+            */
         }
     }
 
@@ -15,7 +31,7 @@ var getCollisionTimes = function(cars) {
     return result
 };
 
-getCollisionTimes([[1,2],[2,1],[4,3],[7,2]])
+getCollisionTimes([[3,4],[5,4],[6,3],[9,1]])
 
 /*
 Input: cars = [[1,2],[2,1],[4,3],[7,2]]
