3afb259028b5bdd16f381e4a2308d70dd8d5b028
Allan5829 <allan.farca@gmail.com>
2021-03-10 23:07:40 -0800

    writes a function that gets the max possible blocks based on initial argument and other useful values.

--- Mar_7_21_to_Mar_13_21/building_boxes/index.js
+++ Mar_7_21_to_Mar_13_21/building_boxes/index.js
@@ -1,6 +1,23 @@
 var minimumBoxes = function(n) {
-    
+    addLevel(0, 1, n)
 };
+
+function addLevel(currentLevel, currentNumber, target) {
+    let newSum = 1
+    let previousValue, nextBlocksOnFloor
+    while (target > newSum) {
+        console.log("current value=", newSum)
+        previousValue = newSum
+        currentLevel++
+        currentNumber += currentLevel + 1
+        newSum = currentNumber + previousValue
+        console.log(currentNumber, newSum)
+    }
+    nextBlocksOnFloor = currentLevel + 1 // if nextBlocksOnFloor + currentNumber + 1 > target then number of blocks placed are only on the floor
+    console.log(nextBlocksOnFloor, currentNumber, newSum)
+}
+
+minimumBoxes(12);
 
 /*
 f(x) = f(x-1) + x + 1 where f(-1) = 0
