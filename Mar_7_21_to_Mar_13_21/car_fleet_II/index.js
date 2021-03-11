0e0e838b99bb8e2fd899637c827dee4492a11d4b
Allan5829 <allan.farca@gmail.com>
2021-03-10 19:34:23 -0800

    creates file with instructions and file to solve problem.

---
+++ Mar_7_21_to_Mar_13_21/car_fleet_II/index.js
@@ -0,0 +1,13 @@
+var getCollisionTimes = function(cars) {
+    console.log(cars)
+};
+
+getCollisionTimes([[1,2],[2,1],[4,3],[7,2]])
+
+/*
+Input: cars = [[1,2],[2,1],[4,3],[7,2]]
+Output: [1.00000,-1.00000,3.00000,-1.00000]
+
+Input: cars = [[3,4],[5,4],[6,3],[9,1]]
+Output: [2.00000,1.00000,1.50000,-1.00000]
+*/
