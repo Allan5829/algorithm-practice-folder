2296fc0086478c1b4b197453d9322f89a444d23a
Allan5829 <allan.farca@gmail.com>
2021-03-11 20:30:27 -0800

    my solution passes the first 3 test cases but fails with time limit exceeded when argument becomes 10^5 in length. Unclear how to adapt own answer in regards to time constraint.

--- Mar_7_21_to_Mar_13_21/count_of_smaller_numbers_after_self/index.js
+++ Mar_7_21_to_Mar_13_21/count_of_smaller_numbers_after_self/index.js
@@ -1,3 +1,19 @@
 var countSmaller = function(nums) {
-    
+    let result = []
+
+    for (let i = 0; i < nums.length; nums.shift()) {
+        if (nums.length === 0) {
+            result.push(0)
+        } else {
+            let counts = 0
+            let copyNums = nums.slice(1)
+            copyNums.forEach( x => {
+                nums[0] > x ? counts++ : null
+            })
+            result.push(counts)
+        }
+    }
+    return result
 };
+
+console.log(countSmaller([5,2,6,1]))
