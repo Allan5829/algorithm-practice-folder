e606991c13020024d93aa7f81e5c3cad408f640c
Allan5829 <allan.farca@gmail.com>
2021-03-15 13:58:42 -0700

    solves algorithm if the inputs are arrays but the instructions state linked lists.

--- Mar_14_21_to_Mar_20_21/add_two_numbers/index.js
+++ Mar_14_21_to_Mar_20_21/add_two_numbers/index.js
@@ -1,17 +1,32 @@
 const addTwoNumbers = function(l1, l2) {
-    let [ result, carry, longestNum] = [ [], 0, longestLength(l1.length, l2.length)]
+    let [ result, carryNum, carry, longestNum] = [ [], 0, false, longestLength(l1.length, l2.length)]
     let [x, y, sum] = [0, 0, 0]
     for (let i = 0; i < longestNum; i++) {
         x = numValue(l1[i])
         y = numValue(l2[i])
+        carry? sum = x + y + carryNum : sum = x + y
+        if ( sum < 10 ) {
+            carry = false
+            result.push(sum)
+        } else {
+            sum = twoDigits(sum)
+            carryNum = sum[0]
+            carry = true
+            result.push(sum[1])
+            i === (longestNum - 1)? result.push(carryNum) : null
+        }
         //console.log(x, y)
     }
     console.log(result)
     return result
 };
 
-const numValue = function(x) {
-    return (x && x > -1)? x : 0 // if value exists and is greater then -1 return it else return 0
+const twoDigits = function(sum) {
+    return sum.toString().split('').map( n => { return parseInt(n, 10)})
+}
+
+const numValue = function(n) {
+    return (n && n > -1)? n : 0 // if value exists and is greater then -1 return it else return 0
 }
 
 const longestLength = function(x, y) { // determine if a longer length exists and return it
@@ -25,5 +40,5 @@ const longestLength = function(x, y) { // determine if a longer length exists an
 }
 
 let [l1, l2] = [ [9,9,9,9,9,9,9], [9,9,9,9] ]
-// 
-if (addTwoNumbers(l1, l2) !== [8,9,9,9,0,0,0,1] ) console.log("false")
+// [8,9,9,9,0,0,0,1]
+addTwoNumbers(l1, l2)
