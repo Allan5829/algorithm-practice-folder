9d5bb775584d3994d75243759251ac7e81379e08
Allan5829 <allan.farca@gmail.com>
2021-03-25 22:56:02 -0700

    creates linked lists using classes and applies functionality with instance methods.

--- data_structures/linked_lists.js
+++ data_structures/linked_lists.js
@@ -7,10 +7,61 @@ Doubly linked list- knows beginning and end(tail) of list
 
 Pros- Less cost to add/remove elements at beginning of list compared to arrays
 Cons- More cost to add/remove elements at end of list compared to arrays (can be allieviated with doubly linked list)
-
-Current example creates a linked list by using arrays as nodes stored in an object.
 */
 
+// Linked list using classes
+class Node {
+    constructor(data, next = null){
+        this.data = data,
+        this.next = next
+    }
+}
+
+class LinkedList{
+    constructor(){
+        this.head = null;
+        this.size = 0;
+    }
+
+    //CRUD functions
+
+    add(data) {
+        let newNode = new Node(data)
+        let currentNode
+
+        if (this.head === null) {
+            this.head = newNode
+        } else {
+            currentNode = this.head
+            while(currentNode.next) {
+                currentNode = currentNode.next
+            }  
+            currentNode.next = newNode  
+        }
+        this.size ++
+    }
+
+    //node information functions
+    nodeAt(index) { // return node at index position given
+        let node = this.head
+        if (index > this.size) return false
+        for(let i = 0; i < index; i++){
+            node = node.next;
+        }
+        return node;
+    }
+
+}
+
+//let node1 = new Node("a")
+let list = new LinkedList()
+//console.log(node1, list)
+list.add("b")
+list.add("c")
+list.add("d")
+console.log(list, list.nodeAt(4))
+
+// Current example creates a linked list by using arrays as nodes stored in an object.
 let firstNode = ['cameron', 123]
 let secondNode = ['sloane', 132]
 let thirdNode = ['ferris', null]
@@ -35,6 +86,6 @@ function insertAfter(node, newnode){
 
 }
 // add crud functions
-console.log(next(firstNode))
-console.log(next(thirdNode))
-console.log(indexAt(head, 2))
+//console.log(next(firstNode))
+//console.log(next(thirdNode))
+//console.log(indexAt(head, 2))
